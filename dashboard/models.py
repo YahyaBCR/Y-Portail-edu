e9b05efb65f3f8f5c2fa40d18a263e271f555b25
Yahya Bouachra <101707463+YahyaBCR@users.noreply.github.com>
2023-08-03 22:49:46 +0100

    creer model Notes

--- dashboard/models.py
+++ dashboard/models.py
@@ -3,7 +3,9 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-#class Notes(models.Model):
-#    user = models.ForeignKey(User, on_delete=models.CASCADE)
-#    title = models.CharField(max_length=200)
-#    description = models.TextField()
+class Notes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+    
