e4eb8a835393fe35e1ffa46d44149e98ee9691e7
Yahya Bouachra <101707463+YahyaBCR@users.noreply.github.com>
2023-08-04 11:35:12 +0100

    affichage de notes

--- dashboard/models.py
+++ dashboard/models.py
@@ -2,10 +2,25 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-
+#notes model
 class Notes(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField()
+
+    #afficher le titre des nptes
+    def __str__(self):
+        return self.title
+
+    #supprimer s 'notess'
+    class Meta:
+        verbose_name = "notes"
+        verbose_name_plural = "notes"
+
+
+
+
+
+
 
     
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 
+from . models import Notes
+
 # Create your views here.
 
 def home(request):
     return render(request,'dashboard/home.html')
 
 def notes(request):
-    return render(request,'dashboard/notes.html')
+    #recuperer les notes
+    notes = Notes.objects.filter(user=request.user)
+    context = {'notes':notes}
+    return render(request,'dashboard/notes.html',context)
