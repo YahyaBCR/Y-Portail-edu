842d57357de543e04962ffb7c79c2acf20f5e4cb
Yahya Bouachra <101707463+YahyaBCR@users.noreply.github.com>
2023-08-05 18:34:04 +0100

    changement dans notes page

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 from . models import Notes
+from . forms import *
+
 
 # Create your views here.
 
@@ -8,7 +10,8 @@ def home(request):
     return render(request,'dashboard/home.html')
 
 def notes(request):
+    form = NotesForm()
     #recuperer les notes
     notes = Notes.objects.filter(user=request.user)
-    context = {'notes':notes}
+    context = {'notes':notes,'form':form}
     return render(request,'dashboard/notes.html',context)
